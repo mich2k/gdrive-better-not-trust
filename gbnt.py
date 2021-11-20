460737df639abbd5dccfa130dcea36f5236f6885
Michele <miky3848@gmail.com>
2021-11-20 11:54:10 +0100

    Update gbnt.py

--- gbnt.py
+++ gbnt.py
@@ -5,28 +5,30 @@ import hashlib
 
 print('Mounting Google Drive...')
 drive.mount('/gdrive')
+i=0
 
-src_path = '/gdrive/MyDrive/Source_Path_Here' #@param {type: 'string'}
+src_path = '/gdrive/MyDrive/src_here' #@param {type: 'string'}
 assert os.path.exists(src_path), f"Source '{src_path}' doesn't exist!"
 
-target_path = '/gdrive/MyDrive/Target_Path_Here' #@param {type: 'string'}
+target_path = '/gdrive/MyDrive/Corsi ZIP/dest_here' #@param {type: 'string'}
 assert os.path.exists(target_path), f"Target '{target_path}' doesn't exist!"
 
-i=0
 
 
 for item1 in os.listdir(src_path):
-  print("Analizzo " + str(i+1) + " elemento")
+  print("Checking " + str(i+1) + " element")
   item2 = os.listdir(target_path)[i]
   with open(src_path + "/" + item1,"rb") as f1:
     with open(target_path + "/" + item2, "rb") as f2:
       bytes = f1.read()
-      hash1 = hashlib.sha256(bytes).hexdigest();
+      hash1 = hashlib.md5(bytes).hexdigest();
       bytes = f2.read()
-      hash2 = hashlib.sha256(bytes).hexdigest();
+      hash2 = hashlib.md5(bytes).hexdigest();
+      print(str(hash1) + " - " + str(hash2) )  
   if hash1 == hash2:
-    print("Compatibili")  
+    print("\n\tChecksum verified")
+  else:
+    print("\n\tCorrupted or different file")
   i=i+1
 
-print("Fine analisi su " + str(i+1) + " elementi")
-
+print(str(i+1) + " elements have been checked")
